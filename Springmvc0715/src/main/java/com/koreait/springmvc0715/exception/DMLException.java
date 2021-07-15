f14953dad8f5379caa11c4af39ee2e6e7821c8e2
xodud2972 <xodud2972@naver.com>
2021-07-15 17:51:51 +0900

    0715

---
+++ Springmvc0715/src/main/java/com/koreait/springmvc0715/exception/DMLException.java
@@ -0,0 +1,16 @@
+package com.koreait.springmvc0715.exception;
+
+//sun에서 제공해주는 exception 만으로는 다양한 예외를 처리할 수 없으므로,
+// RuntimeException을 상속받아 나만의 커스텀 예외를 만들어서 사용해보자.
+public class DMLException extends RuntimeException{
+	
+	// 에러메시지 담을 수 있는 예외
+	public DMLException(String msg) {
+		super(msg);
+	}
+	
+	// 에러 메시지 + 에러 원인도 담을 수 있는 생서자 호출
+	public DMLException(String msg, Throwable e) {
+		super(msg, e);
+	}
+}
---
+++ Springmvc0715/src/main/java/com/koreait/springmvc0715/model/board/repository/MybatisBoardDAO.java
@@ -0,0 +1,46 @@
+package com.koreait.springmvc0715.model.board.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.koreait.springmvc0715.exception.DMLException;
+import com.koreait.springmvc0715.model.domain.Board;
+
+import lombok.Setter;
+
+@Setter
+@Repository
+public class MybatisBoardDAO implements BoardDAO{
+	@Autowired
+	private SqlSessionTemplate sessionTemplate;
+	
+	public List selectAll() {
+		List boardList = sessionTemplate.selectList("Board.selectAll");
+		return boardList;
+	}
+
+	@Override
+	public void insert(Board board) throws DMLException{ //여기서 에러를 처리해버리면, 
+		sessionTemplate.insert("Board.insert",board);
+	}
+
+	@Override
+	public Board select(int board_id) {
+		return sessionTemplate.selectOne("Board.select", board_id);
+	}
+
+	@Override
+	public void update(Board board) throws DMLException{
+		sessionTemplate.update("Board.update",board);
+	}
+
+	@Override
+	public void delete(int board_id) throws DMLException{
+		sessionTemplate.delete("Board.delete", board_id);
+	}
+	
+}
