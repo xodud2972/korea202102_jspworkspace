a2bcc916c2163f5d2377ed0ea759d532168d3a5d
xodud2972 <xodud2972@naver.com>
2021-07-16 17:01:30 +0900

    0716

---
+++ Springvdb/src/main/java/com/koreait/springDB/controller/cllient/BoardController.java
@@ -0,0 +1,21 @@
+package com.koreait.springDB.controller.cllient;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller //스프링의 컴포넌트 스캔의 대상이 되기 위
+public class BoardController {
+
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String test() {
+		System.out.println("클라이언트의 test() 호출");
+		return null;
+		
+	}
+	
+	//게시판 목록 요청을 처리
+	@RequestMapping()
+	
+	
+}
