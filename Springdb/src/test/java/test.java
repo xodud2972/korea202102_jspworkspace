6c5a3f156141e0e5ba6e680bcd2d4b4c4dfdce54
xodud2972 <xodud2972@naver.com>
2021-07-26 17:20:43 +0900

    0726

---
+++ Springdb/src/test/java/test.java
@@ -0,0 +1,22 @@
+
+public class test {
+	public static void main(String[] args) {
+		/*
+		 * for(int i=1; i<10; i++) { for(int j=1; j<10; j++) {
+		 * System.out.println(i+"단 : "+i+"x"+j+"="+i*j); } }
+		 */
+		
+		/*
+		 * int i =1; int sum = 0; for(i =1; i<= 100; i++) { if(i % 2== 1) { sum += i; }
+		 * } System.out.println(sum);
+		 */
+		
+		/*
+		 * int a = 3; int b = 7;
+		 * 
+		 * int x = a; int a = b; int b = x;
+		 */
+		
+				
+	}
+}
