9fb604c4015a905dbab8e28a790d9e3cddb420dc
xodud2972 <xodud2972@naver.com>
2021-07-30 21:12:49 +0900

    0730

--- ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/MemberController.java
+++ ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/MemberController.java
@@ -1,6 +1,19 @@
 package com.koreait.shoppingmall.controller.admin;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 //관리자모드의 회원관리 관련 요청 컨트롤러 
+@Controller
 public class MemberController {
+	
+	//회원관리 목록 요청  
+	@GetMapping("/member/list")
+	public String getList(HttpServletRequest request) {
+		
+		return "admin/member/member_list";//회원목록 페이지 
+	}
 
 }
--- ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/OrderController.java
+++ ShoppingMall/src/main/java/com/koreait/shoppingmall/controller/admin/OrderController.java
@@ -1,6 +1,32 @@
 package com.koreait.shoppingmall.controller.admin;
 
-//관리자모드의 주문관리 관련 요청 컨트롤러 
-public class OrderController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.koreait.shoppingmall.exception.LoginFailException;
+
+//관리자모드의 주문관리 관련 요청 컨트롤러
+@Controller
+public class OrderController {
+	
+	@GetMapping("/order/list")
+	public String getList(HttpServletRequest request) { //HttpServletRequest가 눈에 보이지 않는다고 하여, 매개변수로 넘어오지 않은 것이 아니다..
+		//즉 개발자가 원할때는 매개변수에 HttpServletRequest, HttpSession를 명시할 수 있다..
+		
+		return "admin/order/order_list";
+	}
+	
+	
 }
+
+
+
+
+
+
+
